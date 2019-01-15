df8bc95d7acb1d2c02d8fd552cbae7ca42e89d94
Murgic Enis <e.murgic@go-james.com>
2019-01-15 21:13:30 +0100

    Improving ListView

--- app/src/main/java/com/murgic/top10downloader/FeedAdapter.java
+++ app/src/main/java/com/murgic/top10downloader/FeedAdapter.java
@@ -29,17 +29,39 @@ public class FeedAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = layoutInflator.inflate(layoutResource, parent, false);
-        TextView tvName = (TextView) view.findViewById(R.id.tvName);
-        TextView tvArtist = (TextView) view.findViewById(R.id.tvArtist);
-        TextView tvSummary = (TextView) view.findViewById(R.id.tvSummary);
+        ViewHolder viewHolder;
+
+        if(convertView == null) {
+            convertView = layoutInflator.inflate(layoutResource, parent, false);
+
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+//        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+//        TextView tvArtist = (TextView) convertView.findViewById(R.id.tvArtist);
+//        TextView tvSummary = (TextView) convertView.findViewById(R.id.tvSummary);
 
         FeedEntry currentApp = applications.get(position);
 
-        tvName.setText(currentApp.getName());
-        tvArtist.setText(currentApp.getArtist());
-        tvSummary.setText(currentApp.getSummary());
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
 
-        return view;
+        return convertView;
+    }
+
+    private class ViewHolder {
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder(View v) {
+            this.tvName = v.findViewById(R.id.tvName);
+            this.tvArtist = v.findViewById(R.id.tvArtist);
+            this.tvSummary = v.findViewById(R.id.tvSummary);
+        }
     }
 }
